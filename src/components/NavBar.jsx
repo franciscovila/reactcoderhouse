2b9b0fa1e36160a2448122ce233047ef66feea56
Francisco <franvila50@gmail.com>
2022-09-01 14:33:39 -0300

    components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <>
+            <header style= {styles.container}>
+            <h1> Muebleria Giglio </h1>
+            <nav>
+                <a style={styles.links} href="">Donde encontrarnos</a>
+                <a style={styles.links} href="">Metodos de pago</a>
+                <a style={styles.links} href="">Mas info</a>
+            </nav>
+            </header>
+        </>    
+    )
+}
+
+const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'lightgray',
+
+
+},
+    links: {
+        padding: 10,
+}
+}
+
+
+
+export default NavBar;
